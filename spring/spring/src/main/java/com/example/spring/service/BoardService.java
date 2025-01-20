f566b48e56a03b7907a7fe801e7c699e734946ce
NyeongC <cjfsud23@naver.com>
2025-01-20 18:07:08 +0900

    조회 로직에 Redis 적용하기

--- spring/spring/src/main/java/com/example/spring/service/BoardService.java
+++ spring/spring/src/main/java/com/example/spring/service/BoardService.java
@@ -3,6 +3,7 @@ package com.example.spring.service;
 import com.example.spring.domain.Board;
 import com.example.spring.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -16,6 +17,7 @@ public class BoardService {
     @Autowired
     private BoardRepository boardRepository;
 
+    @Cacheable(cacheNames = "getBoards", key = "'boards:page:' + #page + ':size:' + #size", cacheManager = "boardCacheManager")
     public List<Board> getBoards(int page, int size) {
 
         Pageable pageable = PageRequest.of(page - 1, size);
